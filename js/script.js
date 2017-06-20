9c51e54438e1a1b73c1899ea7f85e9e6674e04c5
Crystal <hello@crystalbohacek.com>
2017-06-20 17:24:10 -0400

    best time and current time display

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ var clickReady = false;
 
 /* Cached elements */
 var backgroundElement = document.getElementById("background");
-var timeTakenElement = document.getElementById("timeTaken");
 var bestTimeElement = document.getElementById("bestTime");
 var placeholderElement = document.getElementById("placeholder")
 
@@ -14,22 +13,41 @@ placeholderElement.innerHTML = "wait for green"
 function makeGreenAppear(){
   backgroundElement.style.backgroundColor = "green";
   start = new Date().getTime();
+//clickReady means that it was clicked correctly while green, not while red. 
   clickReady = true;
 }
 
 function appearAfterDelay(){
-  setTimeout(makeGreenAppear, Math.random() * 6000);
+  setTimeout(makeGreenAppear, Math.random() * 5000);
 }
 
 appearAfterDelay();
 
 backgroundElement.onclick = function() {
+  var end = new Date().getTime();
+  var timeTaken = (end - start) / 1000;  
   
+  //display "clicked too soon" if it is clicked while still red
   if(!clickReady){
     placeholderElement.innerHTML = "clicked too soon!" 
     backgroundElement.style.backgroundColor = "blue";
   } else {
-    placeholderElement.innerHTML = "clicked" 
+  //if it is clicked correctly, display the time and blue background.
+    placeholderElement.innerHTML = "Your time: " + timeTaken + "s" 
     backgroundElement.style.backgroundColor = "blue";
   }
+  
+  //if clicked when green but there is no best time, set a best time.
+  if(clickReady && !bestTime){
+    bestTimeElement.style.dispay = "none";
+    bestTime = timeTaken;
+  } else if (clickReady) {
+    bestTimeElement.innerHTML = bestTime + "s";
+    bestTimeElement.style.display = "unset";
+  //set a new best time if it is smaller than the last:
+    if (timeTaken < bestTime) {
+      bestTime = timeTaken;
+    }
+  }
+  clickReady = false;
 }
