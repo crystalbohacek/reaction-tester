8d06317c508e2ee47471b86966ab8b99dee3ff4c
Crystal <hello@crystalbohacek.com>
2017-06-20 16:27:06 -0400

    changed click element from circles to full-page div

--- js/script.js
+++ js/script.js
@@ -1,83 +1,35 @@
-var reactionTester = {
-  /* Game variables */
-  start: null,
-  bestTime: false,
+/* Game variables */
+var start = new Date().getTime();  
+var bestTime = false;
+var clickReady = false;
 
-  /* Cached elements */
-  circleElement: document.getElementById("shape"),
-  timeTakenElement: document.getElementById("timeTaken"),
-  bestTimeElement: document.getElementById("bestTime"),
+/* Cached elements */
+var backgroundElement = document.getElementById("background");
+var timeTakenElement = document.getElementById("timeTaken");
+var bestTimeElement = document.getElementById("bestTime");
+var placeholderElement = document.getElementById("placeholder")
 
-  /* Functions */
+placeholderElement.innerHTML = "wait for green" 
 
-  getRandomColor: function() {
-      var letters = '0123456789ABCDE';
-      var color = '#';
-      for (var i = 0; i < 6; i++ ) {
-        //creates a random selection of letters and numbers to create a hex color
-          color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-  },
-
-  makeCircleAppear: function(){
-    //choose a random location for the circle within certain parameters
-  	var top = Math.random() * 5;
-  	var left = Math.random() * 800;
-  	var width = (Math.random() * 250) + 50;
-
-  	reactionTester.circleElement.style.backgroundColor = reactionTester.getRandomColor();
-  	reactionTester.circleElement.style.top = top + "px";
-  	reactionTester.circleElement.style.left = left + "px";
-  	reactionTester.circleElement.style.width = width + "px";
-  	reactionTester.circleElement.style.height = width + "px";
-  	reactionTester.circleElement.style.display = "block";
-  
-  	reactionTester.start = new Date().getTime();
-  },
-
-  appearAfterDelay: function (){
-  	setTimeout(reactionTester.makeCircleAppear, Math.random() * 2000);
-  }
-
+function makeGreenAppear(){
+  backgroundElement.style.backgroundColor = "green";
+  start = new Date().getTime();
+  clickReady = true;
 }
 
-reactionTester.appearAfterDelay();
-
-reactionTester.circleElement.onclick = function() {
-  reactionTester.circleElement.style.display = "none";
-
-  var end = new Date().getTime();
-  var timeTaken = (end - reactionTester.start) / 1000;
-  //display different colors where it says "your time":
-  if (timeTaken > 1.5){
-    reactionTester.timeTakenElement.style.color = "red";
-  } else if (timeTaken >= 1){
-    reactionTester.timeTakenElement.style.color = "orange";
-  } else {
-    reactionTester.timeTakenElement.style.color = "green";
-  }
-
-  reactionTester.timeTakenElement.innerHTML = timeTaken + "s";
-
-    //if no best time (ie start of the game), do not display the "best time" element:
-  if (!reactionTester.bestTime){
-    reactionTester.bestTimeElement.style.display = "none";
-    reactionTester.bestTime = timeTaken;
-  } else {
-    //display the best time element when a time exists:
-    reactionTester.bestTimeElement.style.display = "unset";
-    //update best time when it's less than the saved best time.
-    if (timeTaken < reactionTester.bestTime) {
-      reactionTester.bestTime = timeTaken;
-    }
-  }
-  
-  reactionTester.bestTimeElement.innerHTML = reactionTester.bestTime + "s";
-
-  reactionTester.appearAfterDelay();
+function appearAfterDelay(){
+  setTimeout(makeGreenAppear, Math.random() * 6000);
 }
 
-var rt = new reactionTester();
+appearAfterDelay();
 
-rt.makeCircleAppear();
+backgroundElement.onclick = function() {
+  
+  if(!clickReady){
+    placeholderElement.innerHTML = "clicked too soon!" 
+    backgroundElement.style.backgroundColor = "blue";
+  } else {
+    placeholderElement.innerHTML = "clicked" 
+    backgroundElement.style.backgroundColor = "blue";
+  }
+}
